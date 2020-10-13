1d8c329a4c2b1a1057bf65105b82d6706f545736
Michael-Tyler <miketylerabc@gmail.com>
2020-10-13 10:33:42 -0500

    added query by email for customers

--- models/customer.py
+++ models/customer.py
@@ -1,5 +1,14 @@
-class Customer(): 
-    def __init__(self, id, name, address, email, password):
+# class Customer(): 
+#     def __init__(self, id, name, address, email, password):
+#         self.id = id
+#         self.name = name
+#         self.address = address
+#         self.email = email
+#         self.password = password
+
+class Customer():
+
+    def __init__(self, id, name, address, email = "", password = ""):
         self.id = id
         self.name = name
         self.address = address
